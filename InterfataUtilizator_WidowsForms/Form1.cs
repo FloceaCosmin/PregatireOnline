bb03cd2fff92dced9966879634880e49b3e0425b
FloceaCosmin <flocea_cosmin@yahoo.com>
2025-03-31 14:45:36 +0300

    Stilizare

--- InterfataUtilizator_WidowsForms/Form1.cs
+++ InterfataUtilizator_WidowsForms/Form1.cs
@@ -15,9 +15,11 @@ namespace InterfataUtilizator_WindowsForms
 
         private Label lblNume;
         private Label lblMaterii;
+        private Label lblPunctaj;
 
         private Label[] lblsNume;
         private Label[] lblsMaterii;
+        private Label[] lblsPunctaj;
 
         private const int LATIME_CONTROL = 150;
         private const int DIMENSIUNE_PAS_Y = 30;
@@ -46,15 +48,37 @@ namespace InterfataUtilizator_WindowsForms
             lblNume.Width = LATIME_CONTROL;
             lblNume.Text = "Nume Profesor";
             lblNume.Left = DIMENSIUNE_PAS_X;
-            lblNume.ForeColor = Color.DarkBlue;
+            lblNume.Top = 10;
+            lblNume.ForeColor = Color.White;
+            lblNume.BackColor = Color.DarkBlue;
+            lblNume.TextAlign = ContentAlignment.MiddleCenter;
+            lblNume.Font = new Font("Arial", 12, FontStyle.Bold);
+            lblNume.BorderStyle = BorderStyle.FixedSingle;
             this.Controls.Add(lblNume);
 
             lblMaterii = new Label();
             lblMaterii.Width = LATIME_CONTROL;
             lblMaterii.Text = "Materii Predate";
             lblMaterii.Left = 2 * DIMENSIUNE_PAS_X;
-            lblMaterii.ForeColor = Color.DarkBlue;
+            lblMaterii.Top = 10;
+            lblMaterii.ForeColor = Color.White;
+            lblMaterii.BackColor = Color.DarkGreen;
+            lblMaterii.TextAlign = ContentAlignment.MiddleCenter;
+            lblMaterii.Font = new Font("Arial", 12, FontStyle.Bold);
+            lblMaterii.BorderStyle = BorderStyle.FixedSingle;
             this.Controls.Add(lblMaterii);
+
+            lblPunctaj = new Label();
+            lblPunctaj.Width = LATIME_CONTROL;
+            lblPunctaj.Text = "Punctaj Profesori";
+            lblPunctaj.Left = 3 * DIMENSIUNE_PAS_X;
+            lblPunctaj.Top = 10;
+            lblPunctaj.ForeColor = Color.White;
+            lblPunctaj.BackColor = Color.DarkRed;
+            lblPunctaj.TextAlign = ContentAlignment.MiddleCenter;
+            lblPunctaj.Font = new Font("Arial", 12, FontStyle.Bold);
+            lblPunctaj.BorderStyle = BorderStyle.FixedSingle;
+            this.Controls.Add(lblPunctaj);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -68,6 +92,7 @@ namespace InterfataUtilizator_WindowsForms
 
             lblsNume = new Label[nrProfesori];
             lblsMaterii = new Label[nrProfesori];
+            lblsPunctaj = new Label[nrProfesori];
 
             for (int i = 0; i < nrProfesori; i++)
             {
@@ -84,6 +109,14 @@ namespace InterfataUtilizator_WindowsForms
                 lblsMaterii[i].Left = 2 * DIMENSIUNE_PAS_X;
                 lblsMaterii[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsMaterii[i]);
+
+                lblsPunctaj[i] = new Label();
+                lblsPunctaj[i].Width = LATIME_CONTROL;
+                lblsPunctaj[i].Text = profesori[i].Punctaj.ToString();
+                lblsPunctaj[i].Left = 3 * DIMENSIUNE_PAS_X;
+                lblsPunctaj[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                this.Controls.Add(lblsPunctaj[i]);
+
             }
         }
     }
